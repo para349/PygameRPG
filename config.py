5c31b39204b52e5cba085530c1957ba10e63b6f6
para349 <124186113+para349@users.noreply.github.com>
2023-05-26 12:11:54 -0600

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,30 @@ WIN_HEIGHT = 480
 TILESIZE = 32
 FPS = 60
 
-PLAYER_LAYER = 1
+PLAYER_LAYER = 2
+BLOCK_LAYER = 1
+
+PLAYER_SPEED = 3
 
 RED = (255, 0 , 0)
 BLACK = (0, 0, 0)
+BLUE = (0, 0, 225)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBB',
+    'B..................B',
+    'B..................B',
+    'B....BBB...........B',
+    'B..................B',
+    'B........P.........B',
+    'B..................B',
+    'B..................B',
+    'B.....BBB..........B',
+    'B.......B..........B',
+    'B.......B..........B',
+    'B..................B',
+    'B..................B',
+    'B..................B',
+    'BBBBBBBBBBBBBBBBBBBB',
+    
+]
